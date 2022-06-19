c8565b315a298770114b35d43eb4c286c8aac64b
Юлия Галиева <gorcha79@mail.ru>
2022-06-19 14:09:43 +0400

    third commit

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -31,6 +31,7 @@ class User(AbstractUser):
         return self.role == 'moderator'
 
     class Meta:
+        ordering = ['id']
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
         constraints = [
